1a14498f25a154f1615d7ca5826b31e6d06a2343
stevenocean <sharexun@163.com>
2018-05-21 14:12:28 +0800

    Add getLatestFileHash method

--- contracts/ReliableTokens.sol
+++ contracts/ReliableTokens.sol
@@ -8,7 +8,10 @@ contract ReliableTokens {
 
     // Tokens file hash with version.
     // (version, hash)
-    mapping(uint => string) private fileHash;
+    mapping(uint32 => string) private fileHash;
+
+    // Save latest version number.
+    uint32 private latestVersion;
     
     // Some methods can only call by contract owner.
     address private owner;
@@ -24,12 +27,25 @@ contract ReliableTokens {
     }
 
     // Set tokens file hash only by owner.
-    function setFileHash(uint v, string h) public onlyOwner {
+    function setFileHash(uint32 v, string h) public onlyOwner {
         fileHash[v] = h;
+        if (v > latestVersion) {
+            latestVersion = v;
+        }
     }
 
     // Return tokens file hash.
-    function getFileHash(uint v) public view returns(string) {
+    function getFileHash(uint32 v) public view returns(string) {
         return fileHash[v];
+    }
+
+    // Return latest version number.
+    function getLatestVersion() public view returns(uint32) {
+        return latestVersion;
+    }
+
+    // Return latest tokens file hash.
+    function getLatestFileHash() public view returns(string) {
+        return fileHash[latestVersion];
     }
 }
